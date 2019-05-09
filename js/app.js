2024a898ac5131e360b05c69b43bd742d19117b4
Isa <yunjonglyu@gmail.com>
2019-05-09 12:44:16 +0900

    CREATE canvas events

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
-function init(){
+const canvas = document.getElementById('jsCanvas');
+
+let painting = false;
+
+function stopPainting () {
+  painting = false;
 
 }
-init();
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  console.log(x, y);
+}
+function onMouseDown(event) {
+  painting = true;
+}
+function onMouseUp(event) {
+  stopPainting();
+}
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseup", onMouseUP);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
