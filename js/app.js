0b4d17dce87f5cca98d6eb0c79b7a13325e4f883
Isa <yunjonglyu@gmail.com>
2019-05-09 13:06:20 +0900

    CREATE canvas path api

--- js/app.js
+++ js/app.js
@@ -1,25 +1,34 @@
 const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext("2d");
 
+canvas.width = 700;
+canvas.height = 700;
+ctx.strokeStyle = "black"
+ctx.lineWidth = 2.5;
 let painting = false;
 
 function stopPainting () {
   painting = false;
 
 }
+function startPainting () {
+  painting = true;
+}
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-  console.log(x, y);
-}
-function onMouseDown(event) {
-  painting = true;
-}
-function onMouseUp(event) {
-  stopPainting();
+  if(!painting){
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+
+  } else {
+    ctx.lineTo(x,y);
+    ctx.stroke();
+  }
 }
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUP);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
